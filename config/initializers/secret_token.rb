8f4d7773b0a36cb91a2a4c616e9b3e6f31a0146f
ygmaster <ygmaster@gmail.com>
2011-02-09 21:35:42 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mdp::Application.config.secret_token = '833b60faa84f31e0d257b590e0f48a394439b0ba71985e7d1b4c2dc019679562cbe05ccafa26b185690b831893d9ad6bf06875b8d16418432ad632d766c9d789'
