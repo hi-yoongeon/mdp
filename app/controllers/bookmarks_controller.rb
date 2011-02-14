c1ff39cfa236f05007f430a53e0c82ba36d61dae
Jihun, Park <brandnew@matji.com>
2011-02-14 21:39:01 +0900

    asd

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,5 +1,5 @@
 class BookmarksController < ApplicationController
-  before_filter :authentication_required, only => []
+  before_filter :authentication_required, :only => []
   reapond_to :xml, :json
 
 
