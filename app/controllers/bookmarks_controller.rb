3bd9b3b99783bf87ee40caa14da16ff8d83b97c3
username <asky@asky.asky>
2011-02-17 12:56:31 +0900

    수정

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,13 +1,2 @@
 class BookmarksController < ApplicationController
-  before_filter :authentication_required, :only => [:new]
-  respond_to :xml, :json
-
-  def new
-
-  end
-
-  def list
-
-  end
-
 end
