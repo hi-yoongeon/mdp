be7e3dbfd2a35a5ff9ed33dddb589f996d1e9867
username <asky@asky.asky>
2011-02-17 15:24:33 +0900

    modify

--- app/models/store_food.rb
+++ app/models/store_food.rb
@@ -4,6 +4,6 @@ class StoreFood < ActiveRecord::Base
   belongs_to :store, :class_name => "Store", :foreign_key => "store_id"
   belongs_to :food, :class_name => "Food", :foreign_key => "food_id"
 
-  validates_presence_of :food_id, :store_id, :food_name
+  validates_presence_of :food_id, :store_id
 
 end
