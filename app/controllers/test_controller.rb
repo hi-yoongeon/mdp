f9028fd4fc7d1d3adf10271024d416c78cc39b36
username <asky@asky.asky>
2011-03-04 01:20:39 +0900

    img test

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -6,39 +6,17 @@ class TestController < ApplicationController
     mfcm = MatjiFileCacheManager.new(100000002)
     
     mfcm.add_follower(10000002) 
-    # mfcm.add_follower(10000003)
-    # mfcm.add_follower(10000004)
-    # mfcm.add_follower(10000005)
-    # mfcm.add_follower(100000234)
-    # mfcm.add_follower(100023234002)
-    # mfcm.add_follower(1000001231132)
     t = mfcm.follower
 
     render :text => t    
  #   render :text => RAILS_ROOT
   end
 
+
   def upload
-
     uploaded_file = params[:upload_file]
-
-
-    unless uploaded_file.nil? do
-
-        require "matji_file_cache_manager"
-        
-        mfcm = MatjiFileCacheManager.new(100000002)
-        mfcm.add_profile_img(uploaded_file)
-
-      end
-
-
-
-
-
-
-
-
+    require "matji_file_cache_manager"
+    mfcm = MatjiFileCacheManager.new(100000002)
+    mfcm.add_profile_img(uploaded_file)
   end
-
 end
