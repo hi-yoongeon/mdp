51f22a1a2a1348d9e783f384a6be2dc87e8f4f90
ygmaster <ygmaster@gmail.com>
2011-03-04 15:47:40 +0900

    modify test controller

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -15,30 +15,8 @@ class TestController < ApplicationController
 
   def upload
     uploaded_file = params[:upload_file]
-<<<<<<< HEAD
-    
-    puts uploaded_file.read
-
-
-    # unless uploaded_file.nil? do
-
-    #     require "matji_file_cache_manager"
-        
-    #     mfcm = MatjiFileCacheManager.new(100000002)
-    #     mfcm.add_profile_img(uploaded_file)
-
-    #   end
-
-
-
-
-
-
-
-=======
     require "matji_file_cache_manager"
     mfcm = MatjiFileCacheManager.new(100000002)
     mfcm.add_profile_img(uploaded_file)
->>>>>>> 0605ccc8fb922cd5de2c8b96762a55fd91a78042
   end
 end
