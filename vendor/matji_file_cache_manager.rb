345f9ae3e03e284449d1295b22f4e11012d565d6
username <asky@asky.asky>
2011-03-07 17:56:06 +0900

    modify

--- vendor/matji_file_cache_manager.rb
+++ vendor/matji_file_cache_manager.rb
@@ -23,6 +23,7 @@ class MatjiFileCacheManager
     add_user(filename, user_id)
   end
 
+
   def remove_follower(user_id)
     filename = @user_path + "/follower"
     remove_user(filename, user_id)
@@ -44,16 +45,17 @@ class MatjiFileCacheManager
 
   def add_profile_img(img)
     timestamp = Time.new.to_i
-    filename = "profile_img_original/#{timestamp}"
+    filename = @user_path + "/profile_img_original/#{timestamp}"
     File.new(filename, "w") unless File.exist? filename
     File.open(filename, "wb") do |f|
       f.write img.read
     end    
     require 'RMagick'
-    thum_img = Magick::ImageList.new("profile_img_original/#{timestamp}")
+    thum_img = Magick::ImageList.new("#{@user_path}/profile_img_original/#{timestamp}")
     thum_img.resize!(48,48)
-    thum_img.write("profile_img_thumnail/#{timestamp}")
+    thum_img.write("#{@user_path}/profile_img_thumnail/#{timestamp}")
   end
+
 
   def follower
     filename = @user_path + "/follower"
@@ -69,8 +71,10 @@ class MatjiFileCacheManager
 
 
   def profile_img
-    filename = @user_path + "/image"
+    filename = @user_path + "/profile_img_thumnail"
+    
   end
+
 
   private
 
@@ -80,16 +84,13 @@ class MatjiFileCacheManager
       unless (text = f.read.chomp).nil? 
         arr = text.split(",")
       end
-      puts arr.size
     end
     arr.push(user_id.to_s).uniq!
-
     File.open(filename, File::RDWR|File::TRUNC) { |f| 
-      puts arr.join(",")
       f.write arr.join(",") + "\n" 
     }
-
   end
+
 
   def remove_user(filename, user_id)
     arr = Array.new()
@@ -98,24 +99,21 @@ class MatjiFileCacheManager
     File.open(filename, File::RDWR|File::TRUNC) { |f| f.write arr.join(",") + "\n" }
   end
 
+
   def user_dir
     "file_cache/user/" + @user_id[0..1]+"/"+@user_id[2..3]+"/"+@user_id[4..5]+"/"+@user_id[6..7]+"/"+@user_id[8..9]
   end
 
+
   def make_file_cache_dir
     puts "#{@user_id}에 맞게 디렉토리 생성";
     ud = user_dir.split("/")
-
     ud.each do |w| 
       unless w == ""      
         Dir.mkdir(w) unless Dir.exist? w
         Dir.chdir(w)
       end
     end
-    # ori_img_path = "#{@user_path}/profile_img_original" 
-    # thum_img_path = "#{@user_path}/profile_img_thumbnail"
-    # Dir.mkdir(ori_img_path) unless Dir.exist? ori_img_path
-    # Dir.mkdir(thum_img_path) unless Dir.exist? thum_img_path
     Dir.mkdir("profile_img_original") unless Dir.exist? "profile_img_original"
     Dir.mkdir("profile_img_thumnail") unless Dir.exist? "profile_img_thumnail"
   end
